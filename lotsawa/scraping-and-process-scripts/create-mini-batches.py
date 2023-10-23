282213286f0c39d3e26bb9a15660415405c187c9
Jacob Moore <billingsmoore@gmail.com>
2023-10-23 16:20:45 -0400

    created streamlined process for training large set

--- lotsawa/scraping-and-process-scripts/create-mini-batches.py
+++ lotsawa/scraping-and-process-scripts/create-mini-batches.py
@@ -1,18 +1,14 @@
-def process(num):
+def process():
     entries = []
     line_count = 0
     batch_count = 0
-    with open('/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/ankhi/bing-batches/batch'+str(num)+'.txt', 'r') as f:
+    with open('/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/all-pairs.txt', 'r') as f:
         for line in f:
-            entries.append(line)
             line_count += 1
-            if line_count >= 100:
-                batch_path = '/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/ankhi/bing-batches/mini-batches/'+str(num)+'/' + str(batch_count) + '.txt'
-                with open(batch_path, 'w') as g:
-                    g.write(''.join(entries))
-                entries = []
-                line_count = 0
-                batch_count += 1
+            batch_count = line_count // 500000
+            batch_path = '/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/training-batches/training-batch-' + str(batch_count) + '.txt'
+            with open(batch_path, 'a') as g:
+                g.write(line)
+                
 
-for i in range(109,119):
-    process(i)
+process()
