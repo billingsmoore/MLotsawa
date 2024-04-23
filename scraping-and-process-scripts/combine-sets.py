6cbafa3f580d64ac7f3b3f867d9379d3dca5d048
Jacob Moore <billingsmoore@gmail.com>
2024-04-22 22:18:51 -0400

    cleaned up notebooks for documentation

--- scraping-and-process-scripts/combine-sets.py
+++
@@ -1,4 +0,0 @@
-with open('/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/ankhi/all-ankhi-pairs.txt', 'r') as f:
-    with open('/home/j/Documents/Projects/Iron-Bridge/lotsawa/data/all-pairs.txt', 'a') as g:
-        for line in f:
-            g.write(line)
