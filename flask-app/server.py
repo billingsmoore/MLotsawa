5611a0a1ac30a1bd9bd9702bc4d07f15448b5042
billingsmoore <billingsmoore@gmail.com>
2024-08-28 14:45:48 -0400

    auto open browser window

--- flask-app/server.py
+++ flask-app/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from utils.translate import translate
+from waitress import serve
+import webbrowser
+from threading import Timer
 
 app = Flask(__name__, static_folder='static')
 
@@ -7,10 +10,18 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template('index.html', input='Enter phonetic Tibetan here.', translation='Translation will appear here.')
 
+
 @app.route('/translate', methods=["POST", "GET"])
 def serve_translation():
     input = request.form['input']
     translation = translate(input)
     return render_template('index.html', input=input, translation=translation)
 
-app.run(host='0.0.0.0', port=5000)
+
+def open_browser():
+      webbrowser.open_new("http://127.0.0.1:5000")
+
+
+if __name__ == '__main__':
+    Timer(1, open_browser).start()
+    serve(app, host="0.0.0.0", port=5000, url_scheme='https')
