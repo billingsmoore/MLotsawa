ae10f7ea6adda9c405edba1b342efbe428eae339
billingsmoore <billingsmoore@gmail.com>
2024-08-28 14:44:22 -0400

    uses gpu if available

--- flask-app/utils/translate.py
+++ flask-app/utils/translate.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 from  thl_phonetic_transliteration.converter import convert
 
 model = 'billingsmoore/mlotsawa'
-translator = pipeline('translation', model=model)
+translator = pipeline('translation', model=model, device_map='auto')
 
 def translate(input):
 
