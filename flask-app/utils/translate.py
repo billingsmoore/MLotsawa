26fcf439907548a0507f0fd61e44a4ae5cd15eac
Jacob Moore <billingsmoore@gmail.com>
2024-09-13 18:10:17 -0400

    Add files via upload

--- flask-app/utils/translate.py
+++ flask-app/utils/translate.py
@@ -1,23 +1,38 @@
 from transformers import pipeline
-from  thl_phonetic_transliteration.converter import convert
+from  thl_phonetic_transliteration.transliterator import Transliterator
 
-model = 'billingsmoore/mlotsawa'
+model = 'billingsmoore/phonetic-tibetan-to-english-translation'
 translator = pipeline('translation', model=model, device_map='auto')
 
 def translate(input):
 
-    # transliterate text
-    input = convert(input)
-    
     # clean input for translation
     input = input.strip()
-    input = input.split('\n')
+    input_lst = input.split('\n')
+
+    for elt in input_lst:
+        if elt=='\r':
+            input_lst.remove(elt)
+        elt.replace('\r', '')
+
+    # transliterate text
+    transliterator = Transliterator()
+
+    phonetic_lst = [transliterator.convert(elt) for elt in input_lst]
 
     # create translation
-    translation = translator(input)
+    translation_lst = translator(phonetic_lst)
+    translation_lst = [elt['translation_text'] for elt in translation_lst]
 
-    # clean translation for display
-    translation = [elt['translation_text'] for elt in translation]
-    translation = '\n'.join(translation)
+    # prepare texts for display
+    output_lst = []
 
-    return translation
+    for i in range(len(input_lst)):
+        combo = [input_lst[i], phonetic_lst[i], translation_lst[i]]
+        combo = '\n'.join(combo)
+        output_lst.append(combo+'\n')
+
+    output = '\n'.join(output_lst)
+    print(output)
+
+    return output
